7ee847f6f644c78f7b4d53a373e9525ac6c432ce
Massimo Ridella <mridella.ieu2022@student.ie.edu>
2025-05-15 13:09:32 +0200

    Fixng home page issue

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Skull from '../components/Skull.vue'
 import AppAccounts from '../components/AppAccounts.vue'
-import Home from '../components/Skull.vue'
+import Home from '../components/Home.vue'
 
 Vue.use(VueRouter)
 
