fad1798ce5ce1335c6fd6a560e94dd4ced28f86c
Massimo Ridella <mridella.ieu2022@student.ie.edu>
2025-05-20 00:07:07 +0200

    added button to direct from homepage to accounts

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,5 +2,20 @@
     <div style = "text-align: center;">
         <h1>Welcome to IE Bank</h1>
         <img src = "@/assets/logo.png" alt="IE Bank Logo" style="max-width: 300px; margin-top: 20px;" />
+        <br>
+        <button @click="GoToAccounts">Go to Dashboard</button>
     </div>
 </template>
+
+<script>
+
+import router from '@/router';
+
+export default {
+  methods: {
+    GoToAccounts() {
+      router.push('/accounts');
+    }
+  }
+};
+</script>
